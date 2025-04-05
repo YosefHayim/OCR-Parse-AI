dcf9130ed6c2d6585c2e26baf619368c35d6b06d
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-05 20:45:12 +0300

    fd

--- client/src/pages/Homepage/Homepage.tsx
+++ client/src/pages/Homepage/Homepage.tsx
@@ -2,21 +2,25 @@ import { useMutation } from "@tanstack/react-query";
 import { postPdfFile } from "../../../api/postPdfFile";
 import { FaFileUpload } from "react-icons/fa";
 import { useState } from "react";
-import Loader from "components/Loader/Loader";
-import Navbar from "components/Navbar/Navbar";
-import Footer from "components/Footer/Footer";
-import { Button } from "components/ui/button";
+import Footer from "@/components/Footer/Footer";
+import Loader from "@/components/Loader/Loader";
+import Navbar from "@/components/Navbar/Navbar";
+import { Button } from "@/components/ui/button";
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
 
 const Homepage = () => {
   const [isLoading, setLoading] = useState(false);
   const mutatePdfFile = useMutation({
     mutationFn: postPdfFile,
     onSuccess: (data) => {
-      console.log(data);
+      toast("קבצים בתהליך ניתוח נתונים...");
       setLoading(false);
     },
     onError: (error) => {
       console.log(error);
+      toast("שגיאה בהעלאת הקובץ");
+
       setLoading(false);
     },
   });
@@ -24,20 +28,24 @@ const Homepage = () => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const file = formData.get("file") as File;
-
     mutatePdfFile.mutate(file);
   };
+
   return (
     <div className="w-full">
       <Navbar />
+      <Toaster position="top-center" />
       {isLoading ? (
-        <div></div>
+        <div className="flex w-full items-center justify-center">
+          <Loader />
+        </div>
       ) : (
         <form className="flex w-full flex-col gap-4" onSubmit={handleSubmit}>
           <div className="flex w-full flex-col gap-2 bg-white p-4">
             <div className="w-full text-right">
               <label htmlFor="file">העלאה קובץ</label>
             </div>
+
             <div className="rounded-lg border border-dotted bg-gray-200 p-4">
               <div className="flex w-full flex-col items-center justify-center gap-2">
                 <label htmlFor="file">
@@ -54,7 +62,7 @@ const Homepage = () => {
                   />
                   <div className="pointer-events-none w-full rounded border px-4 py-2 text-center">
                     <span id="fileLabel" className="text-gray-500">
-                      לחצי על האייקון או הכפתור להעלאת קובץ
+                      לחץ על האייקון או הכפתור להעלאת קובץ
                     </span>
                   </div>
                 </div>
@@ -75,9 +83,6 @@ const Homepage = () => {
           </div>
         </form>
       )}
-      <div className="flex w-full items-center justify-center">
-        <Loader />
-      </div>
       <Footer />
     </div>
   );
