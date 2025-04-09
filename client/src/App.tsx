8b041f22e58d3ea39a0da6cee872946e0f5b86f0
yosefhayim <yosefisabag+03@gmail.com>
2025-04-10 00:07:56 +0300

    installed socket io on client and server working on connection both

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,22 @@
 import AppRoutes from "./routes/routes";
-import "./App.css";
+import { io } from "socket.io-client";
+import "@/App.css";
+
 const App = () => {
+  const socket = io();
+
+  socket.on("connect", () => {
+    console.log(socket.connected);
+  });
+
+  socket.on("progress-of-extraction", () => {
+    if (socket.connected) {
+      socket.emit("progress-of-extraction", () => {
+        "Client is connected and ready to recieve progress of extraction pdf";
+      });
+    }
+  });
+
   return (
     <div className="w-full">
       <AppRoutes />
